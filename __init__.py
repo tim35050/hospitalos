d14c68261896cb19509a228f072272cd70bbf7a2
Tim <tim35050@gmail.com>
2013-11-11 18:46:11 -0800

    Mon 6:46PM

--- __init__.py
+++ __init__.py
@@ -7,29 +7,32 @@ def index():
 	context = []
 	# Assume 3 vitals for now
 	vital = {}
+	vital['vital_id'] = 'bmi'
+	vital['vital_name'] = 'BMI'
+	vital['vital_icon_name'] = 'bmi-icon.png'
+	vital['vital_values'] = [35]
+	vital['vital_values_sep'] = ''
+	vital['vital_unit'] = 'kg/m&#178;'
+	vital['vital_risk'] = 'high-risk'
+	context.append(vital)
+
+	vital = {}
 	vital['vital_id'] = 'bloodpressure'
 	vital['vital_name'] = 'Blood pressure'
 	vital['vital_icon_name'] = 'bloodpressure-icon.png'
-	vital['vital_value'] = '130/90'
+	vital['vital_values'] = [130, 90]
+	vital['vital_values_sep'] = '/'
 	vital['vital_unit'] = 'mmHg'
-	vital['vital_risk'] = 'high-risk'
+	vital['vital_risk'] = 'medium-risk'
  	context.append(vital)
 
 	vital = {}
 	vital['vital_id'] = 'bloodglucose'
 	vital['vital_name'] = 'Blood glucose level'
 	vital['vital_icon_name'] = 'bloodglucose-icon.png'
-	vital['vital_value'] = '100'
+	vital['vital_values'] = [100]
+	vital['vital_values_sep'] = ''
 	vital['vital_unit'] = 'mg/dL'
-	vital['vital_risk'] = 'medium-risk'
-	context.append(vital)
-
-	vital = {}
-	vital['vital_id'] = 'bmi'
-	vital['vital_name'] = 'BMI'
-	vital['vital_icon_name'] = 'bmi-icon.png'
-	vital['vital_value'] = '20'
-	vital['vital_unit'] = 'kg/m&#178;'
 	vital['vital_risk'] = 'low-risk'
 	context.append(vital)
 
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -47,9 +47,16 @@ $( ".vital" ).click(function() {
 	var vo = $(this).children('.vital-overlay');
     vo.fadeIn(500, function() {
     	voc = vo.children('.vital-overlay-content');
-    	classes = voc.children('.vital-stats').attr('class');
-    	vital = classes.split(' ')[1]
-    	animateVital(vital);
+    	classes = voc.children('.vital-stats').attr('class').split(' ');
+    	vital = classes[1];
+    	vitalRisk = classes[2];
+    	vov = voc.children('.vital-overlay-value');
+    	var vitalValues = [];
+    	vov.children().each(function() {
+    		vitalValues.push($(this));
+    	});
+    	animateVital(vital, vitalRisk, vitalValues);
+		displayHistogram(vital);
     });
 });
 
@@ -58,12 +65,56 @@ $( ".close-overlay" ).click(function() {
 	event.stopPropagation();
 });
 
-function animateVital(vital) {
-	img = d3.select('.' + vital + ' image');
-	console.log(img);
-	img.transition()
-		.attr("x", 100);
+var animatedVitals = [];
+
+function animateVital(vital, vitalRisk, values) {
+	// the following doesn't work for IE 8 and below
+	if (animatedVitals.indexOf(vital) == -1) {
+		imgUrl = getSmileyUrl(vitalRisk);
+		index = 0;
+		scaled_values = []
+		index = 0;
+		d3.selectAll('.' + vital + ' svg').each(function() {
+			svg = d3.select(this);
+			svg.append("image")
+				.attr("x", padding - smileyWidth/2)
+				.attr("y", barYPos - smileyHeight)
+				.attr("xlink:href", imgUrl)
+				.attr("src", imgUrl)
+				.attr("width", smileyWidth)
+				.attr("height", smileyHeight)
+				.attr("class", svg.attr("class"));
+		});
+		
+		d3.selectAll('.' + vital + ' image').each(function() {
+			img = d3.select(this);
+			vitalValue = values[index].html();
+			className = img.attr("class");
+			scaledValue = scaleFunctions[vital + '-' + className](vitalValue);
+		 	img.transition()
+		 		.duration(1000)
+		 		.attr("x", scaledValue - smileyWidth/2);
+		 	index += 1;
+		});
+		animatedVitals.push(vital);
+	}
 }
+
+function getSmileyUrl(vitalRisk) {
+	baseUrl = '../static/css/images/smiley-';
+	return baseUrl + vitalRisk + '.png'
+}
+
+// D3 Variables
+var scaleFunctions = {};
+var w = 550;
+var h = 80;
+var barSpacing = 2;
+var barHeight = 14;
+var barYPos = 42;
+var padding = 25;
+var smileyWidth = 33;
+var smileyHeight = 36;
 
 function loadD3Vitals() {
 
@@ -75,22 +126,11 @@ function loadD3Vitals() {
 		for (j = 0; j < vital.dataset.length; j++) {
 			dataset = vital.dataset[j];
 
-			var w = 500;
-			var h = 80;
-			var barSpacing = 2;
-			var barHeight = 14;
-			var barYPos = 42;
-			var padding = 25;
-			var cumLength = 0;
-
-			// for (k=0; k < dataset.value.length; k++) {
-			// 	w += dataset.value[k][0] + barSpacing;
-			// }
-
 			var svg = d3.select('.' + vital.id)
 						.append("svg")
 						.attr("width", w)
-						.attr("height", h);
+						.attr("height", h)
+						.attr("class", dataset.name);
 
 			var xScalePos = d3.scale.linear()
 									.domain([dataset.min, dataset.max])
@@ -99,6 +139,9 @@ function loadD3Vitals() {
 			var xScaleVal = d3.scale.linear()
 									.domain([0, dataset.max - dataset.min])
 									.range([padding, w - padding]);
+
+			scaleFunctions[vital.id + '-' + dataset.name] = xScalePos;
+			var cumLength = 0;
 
 			svg.selectAll("rect")
 				.data(dataset.value)
@@ -124,16 +167,6 @@ function loadD3Vitals() {
 				.attr("y", barYPos - 4)
 			    .text(dataset.name);
 
-			var imageWidth = 33;
-			var imageHeight = 36
-			svg.append("image")
-				.attr("x", padding - imageWidth/2)
-				.attr("y", barYPos - imageHeight)
-				.attr("xlink:href", "../static/css/images/smiley-neutral.png")
-				.attr("src", "../static/css/images/smiley-neutral.png")
-				.attr("width", imageWidth)
-				.attr("height", imageHeight);
-
 			var xAxis = d3.svg.axis()
 								.scale(xScalePos)
 								.tickValues(xScalePos.domain())
@@ -144,6 +177,110 @@ function loadD3Vitals() {
 					.call(xAxis);
 		}
 	}
+}
+
+// Histogram
+function displayHistogram(vital) {
+	d3.csv("../static/data/sys-bp.csv", function(data) {
+    	console.log(data);
+    	dataset = data.map(function(d) { return parseInt(d["Systolic BP"]); });
+    	generateVis(vital);
+	});
+}
+
+function generateVis(vital) {
+
+    var formatCount = d3.format(",.0f");
+	var formatAsPercentage = d3.format("1%");
+    var margin = {top: 30, right: 30, bottom: 40, left: 30},
+        width = 550 - margin.left - margin.right,
+        height = 350 - margin.top - margin.bottom;
+
+    minData = d3.min(dataset, function(d) { return d; });
+    minData = Math.max(minData, 70);
+    maxData = d3.max(dataset, function(d) { return d; });
+    maxData = Math.min(maxData, 190);
+
+
+    var xData = d3.scale.linear()
+        .domain([ minData , maxData ])
+        .range([ 0, width ]);
+
+    var x = d3.scale.linear()
+        .domain([ 0, maxData - minData ])
+        .range([ 0, width ]);
+
+    data = d3.layout.histogram()
+        .bins(xData.ticks(40))
+        (dataset);
+
+    var y = d3.scale.linear()
+        .domain([0, d3.max(data, function(d) { 
+            return d.y / dataset.length; 
+        })])
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(xData)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .tickFormat(formatAsPercentage);
+
+    var svg = d3.select('.' + vital).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      	.append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var bar = svg.selectAll(".bar")
+        .data(data)
+        .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { 
+            return "translate(" + xData(d.x) + "," + y(d.y / dataset.length) + ")"; 
+        });
+
+    bar.append("rect")
+        .attr("x", 1)
+        .attr("width", x(data[0].dx) - 1)
+        .attr("height", function(d) {
+            return height - y(d.y / dataset.length); 
+        })
+        .attr("fill", function(d) {
+            xBucket = d.x + d.dx;
+            if (xBucket <= 90) {
+                return "#4BC0B5";
+            } else if(xBucket <= 120) {
+                return "#78BB66";
+            } else if(xBucket <= 140) {
+                return "#F7A649";
+            } else if(xBucket <= 160) {
+                return "#F07B7F";
+            } else {
+                return "#ED5A69";
+            }
+        });
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    svg.append("text")
+        .attr("class", "x label")
+        .attr("text-anchor", "middle")
+        .attr("x", width / 2)
+        .attr("y", height + 35)
+        .text("SYSTOLIC BLOOD PRESSURE (mmHg)");
+
+
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
 }
 
 function getVitalRanges() {
@@ -210,11 +347,3 @@ function getVitalRanges() {
 		];
 	return vr;
 }
-
-//$.ajax({
-//  url: "test.html",
-//  cache: false
-//})
-//  .done(function( html ) {
-//    $( "#results" ).append( html );
-//  });
