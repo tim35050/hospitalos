2764c2d3cea6457da38a83e7a8596f31a5d67035
Tim <tim35050@gmail.com>
2013-11-17 13:51:26 -0800

    Sun 1:51PM

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -61,4 +62,5 @@ def diagnosis():
 	return render_template('diagnosis.html')
 
 if __name__ == '__main__':
-    app.run()
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
